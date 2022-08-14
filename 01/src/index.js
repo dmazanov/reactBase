3c8cbc16b7671933100ff65b3b0c6cab08d7a9e6
Denys <d.mazanov@global4net.com>
2022-08-14 20:05:49 +0200

    create div with to inner heading

--- 01/src/index.js
+++ 01/src/index.js
@@ -1,9 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const App = React.createElement(
+    'div',
+    {
+        className: 'wrapper',
+    },
+    [
+        React.createElement(
+          'h1',
+            {
+                className: 'mainHeading',
+            },
+          'Hello world'
+        ),
+        React.createElement(
+            'h2',
+            {
+                className: 'secondaryHeading',
+            },
+            'Hello react'
+        )
+    ]
+);
+
 ReactDOM.render(
     //rendered element
-  <h1>Hello world</h1>,
+    App,
     // target
   document.getElementById('root')
 )
