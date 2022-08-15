106c7b274914fe18f666067a7130a6e2166756fb
Denys <d.mazanov@global4net.com>
2022-08-15 23:07:35 +0200

    added styles for ListItem component

--- 02/src/components/ListWrapper/ListItem/ListItem.js
+++ 02/src/components/ListWrapper/ListItem/ListItem.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import './ListItem.css';
+import danAbromovImage from '../../../assets/images/danabramov.jpg';
 
 const ListItem = () => (
-    <li className="listItemWrapper">
-        item1
+    <li className="listItem__wrapper">
+        <img src={danAbromovImage} className="listItem__image" />
+        <div>
+            <h2 className="listItem__name">Dan Abramov</h2>
+            <p className="listItem__description">It is a good idea to think of your PC as an office. It stores files, programs, pictures. This can be compared to an actual office's files, machines and decorations.</p>
+            <button className="listItem__button">visit twitter page</button>
+        </div>
     </li>
 );
 
