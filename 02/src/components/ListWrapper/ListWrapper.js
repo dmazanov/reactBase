926a5973f41dfc7d6c41545c84a8f5a27e7f9c35
Denys <d.mazanov@global4net.com>
2022-08-15 22:44:01 +0200

    created private component and added styles for it

--- 02/src/components/ListWrapper/ListWrapper.js
+++ 02/src/components/ListWrapper/ListWrapper.js
@@ -1,10 +1,9 @@
 import React from 'react';
+import ListItem from './ListItem/ListItem';
 
 const ListWrapper = () => (
     <ul>
-        <li>list item 1</li>
-        <li>list item 2</li>
-        <li>list item 3</li>
+        <ListItem />
     </ul>
 );
 
