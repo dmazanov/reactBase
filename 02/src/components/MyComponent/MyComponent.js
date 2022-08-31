ab32f4eb5d6475d7da2ca070392556cb6eb8aa53
Denys <d.mazanov@global4net.com>
2022-08-31 23:26:31 +0200

    modified state of a component

--- 02/src/components/MyComponent/MyComponent.js
+++ 02/src/components/MyComponent/MyComponent.js
@@ -2,12 +2,21 @@ import React from 'react';
 
 class MyComponent extends React.Component {
     state = {
-        text: 'test state',
+        text: '',
     }
+
+    handleChange = (event) => {
+        this.setState({text: event.target.value.toUpperCase() });
+    }
+
     render() {
         return (
             <>
-                <input placeholder="your text" />
+                <input
+                    placeholder="Your text"
+                    onChange={this.handleChange}
+                    value={this.state.text}
+                />
                 <h1>{this.state.text}</h1>
             </>
         )
