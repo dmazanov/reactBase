06772669608d8a0c020ed8206bcbc547f3240737
Denys Mazanov <dmazanov@mail.ru>
2023-01-06 20:46:34 +0100

    added Navigation component

--- 04/src/views/Root/Root.js
+++ 04/src/views/Root/Root.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ArticlesView from "../ArticlesView/ArticlesView";
 import NotesView from "../NotesView/NotesView";
 import TwittersView from "../TwittersView/TwittersView";
+import Navigation from "../../components/Navigation/Navigation";
 
 const initialStateItems = [
   {
@@ -38,6 +39,7 @@ class Root extends React.Component {
   render() {
     return (
       <BrowserRouter>
+        <Navigation />
         <Routes>
           <Route exact path="/" element={<TwittersView  />} />
           <Route path="/articles" element={<ArticlesView />} />
