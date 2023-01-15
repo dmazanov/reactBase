2940ac46d4fe116ae3e45343d6df273b3170ae5e
Denys Mazanov <dmazanov@mail.ru>
2023-01-15 14:51:05 +0100

    added styles for header

--- 04/src/components/Button/Button.js
+++ 04/src/components/Button/Button.js
@@ -1,25 +1,41 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styles from './Button.module.scss';
 
-const Button = ({ children, href }) => (
-  <>
-    {
-      href ? (
-        <a
-          href={href}
-          target="_blank"
-          className={styles.button}
-          rel="noopener noreferrer"
-        >
-          {children}
-        </a>
-      ) : (
-          <button className={styles.button}>
+const Button = ({ children, href, secondary }) => {
+
+  const buttonClass = secondary ? styles.secondary : styles.button
+
+  return (
+    <>
+      {
+        href ? (
+          <a
+            href={href}
+            target="_blank"
+            className={buttonClass}
+            rel="noopener noreferrer"
+          >
             {children}
-          </button>
-        )
-    }
+          </a>
+        ) : (
+            <button className={buttonClass}>
+              {children}
+            </button>
+          )
+      }
   </>
-);
+  )
+};
+
+Button.propTypes = {
+  children:PropTypes.node.isRequired,
+  href: PropTypes.string,
+  secondary: PropTypes.bool,
+};
+
+Button.defaultProps = {
+  secondary: false,
+};
 
 export default Button;
